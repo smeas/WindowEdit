d8b06d3453e9df8e5eef7aab574eaf7436602d05
Jonatan Johansson <smeas@users.noreply.github.com>
2024-10-19 23:05:14 +0200

    Version 1.3

--- Window Edit/Properties/AssemblyInfo.cs
+++ Window Edit/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Window Edit")]
-[assembly: AssemblyCopyright("Copyright © Gräddsko 2019")]
+[assembly: AssemblyCopyright("Copyright © Gräddsko 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -53,5 +53,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
